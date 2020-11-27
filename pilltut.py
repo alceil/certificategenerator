71d59c3e2b8cf8129cbdcd16e18ee17a427c7ae6
alceil <ashishshajisrampickal@gmail.com>
2020-11-27 16:54:24 +0530

    added feature to  read from excels

--- pilltut.py
+++ pilltut.py
@@ -1,9 +1,23 @@
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
-image_1 = Image.open('UX Certificate.png')
-# image_1.show()
-draw = ImageDraw.Draw(image_1)
-font = ImageFont.truetype("arial.ttf",60)
-draw.text((361,356),'Ashish Thomas',(0,0,0),font=font)
-image_1.save('ashish.png')
+import pandas as pd
+
+
+def creatcert(name:str):
+    image_1 = Image.open('UX Certificate.png')
+    draw = ImageDraw.Draw(image_1)
+    font = ImageFont.truetype("arial.ttf",60)
+    draw.text((361,356),name,(0,0,0),font=font)
+    image_1.save(f'cert/{name}.png')
+
+dataframe = pd.read_excel('data.xlsx')
+for i in dataframe.index:
+    entry = dataframe.iloc[i]
+    name = entry['Full Name']
+    creatcert(name)
+
+
+
+
+
